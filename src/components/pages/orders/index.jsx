536169a49d8486bbe61c862a69e11c5917549f2b
Alex Surya <alexsuryaras@gmail.com>
2020-05-10 11:02:14 +0530

    added tabs

--- src/components/pages/orders/index.jsx
+++ src/components/pages/orders/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import { Breadcrumb, Row, Col, Button, Divider } from "antd";
+import { Breadcrumb, Row, Col, Button, Divider, Tabs, Menu } from "antd";
 import {
   HomeOutlined,
   HeartFilled,
@@ -7,11 +7,16 @@ import {
   ClockCircleOutlined,
 } from "@ant-design/icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWallet, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { MailOutlined, AppstoreOutlined, SettingOutlined } from '@ant-design/icons';
+
+import { faWallet, faAngleDown, faLifeRing } from "@fortawesome/free-solid-svg-icons";
 import CommonHeader from "../../layout/header";
 import Circle from "../../circle";
 import labels from "../../../config/static/lables";
 import dish from "../../../assets/images/dish.png";
+
+const { TabPane } = Tabs;
+const { SubMenu } = Menu;
 
 const label = labels.en;
 
@@ -20,7 +25,7 @@ class Orders extends Component {
     super(props);
     this.state = {};
   }
-  componentDidMount() {}
+  componentDidMount() { }
 
   renderHeader = () => {
     return (
@@ -77,7 +82,7 @@ class Orders extends Component {
             <Col span={14} offset={2}>
               <div className="card-view">
                 <Row justify="space-between" align="middle">
-                  <Col>
+                  <Col className="justify-center">
                     <Row justify="space-around" align="middle">
                       <FontAwesomeIcon icon={faWallet} />
                       <span>25 TL</span>
@@ -86,11 +91,16 @@ class Orders extends Component {
                     <p className="note">Min.Paket tutari</p>
                   </Col>
                   <Divider type="vertical" style={{ height: 35, width: 3 }} />
-                  <Col>
-                    <h3>sdsd</h3>
+                  <Col className="justify-center">
+                    <Row justify="space-around" align="middle">
+                      <FontAwesomeIcon icon={faLifeRing} />
+                      <span>25 TL</span>
+                      <FontAwesomeIcon icon={faAngleDown} />
+                    </Row>
+                    <p className="note">Ort.Servis sursei</p>
                   </Col>
                   <Divider type="vertical" style={{ height: 35, width: 3 }} />
-                  <Col span={8}>
+                  <Col span={6} className="justify-space-between" align='middle'>
                     <Row justify="space-around" align="middle">
                       <ClockCircleOutlined />
                       <span>09:00 - 22:00</span>
@@ -107,6 +117,72 @@ class Orders extends Component {
     );
   };
 
+  filterView = () => {
+    return (
+      <div className="bg-gray no-radius">
+        <Row>
+          <Col span={10} >
+            <Menu selectedKeys={[this.state.current]} mode="horizontal">
+              <Menu.Item key="mail">
+                Set Menuler
+        </Menu.Item>
+              <Menu.Item key="app">
+                Baslangiclar
+        </Menu.Item>
+              <SubMenu icon={<SettingOutlined />} title="Navigation Three - Submenu">
+                <Menu.ItemGroup title="Item 1">
+                  <Menu.Item key="setting:1">Option 1</Menu.Item>
+                  <Menu.Item key="setting:2">Option 2</Menu.Item>
+                </Menu.ItemGroup>
+                <Menu.ItemGroup title="Item 2">
+                  <Menu.Item key="setting:3">Option 3</Menu.Item>
+                  <Menu.Item key="setting:4">Option 4</Menu.Item>
+                </Menu.ItemGroup>
+              </SubMenu>
+              <Menu.Item key="alipay">
+                <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+                  Navigation Four - Link
+          </a>
+              </Menu.Item>
+            </Menu>
+            {/* <Row justify="space-around">
+              <Button type="link">Set Menuler</Button>
+              <Button type="link">Baslangiclar</Button>
+              <Button type="link">Atistimalikar</Button>
+              <Button type="link">Et Donerler</Button>
+              <Button type="link">Et Donerler</Button>
+            </Row> */}
+          </Col>
+          <Col span={6} offset={8} >
+            <Row justify="end">
+              <h3>Search</h3>
+
+            </Row>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
+
+  renderTabs = () => {
+    return (
+      <Tabs defaultActiveKey="1">
+        <TabPane tab="Siparis ver" key="1">
+          {this.filterView()}
+        </TabPane>
+        <TabPane tab="Bilgiler" key="2">
+          Content of Tab Pane 2
+      </TabPane>
+        <TabPane tab="Yemek onerileri" key="3">
+          Content of Tab Pane 3
+      </TabPane>
+        <TabPane tab="Yorumlar (25)" key="4">
+          Content of Tab Pane 4
+      </TabPane>
+      </Tabs>
+    )
+  }
+
   render() {
     return (
       <Fragment>
@@ -114,6 +190,7 @@ class Orders extends Component {
           <CommonHeader />
           {this.renderHeader()}
           {this.renderFavourite()}
+          {this.renderTabs()}
         </div>
       </Fragment>
     );
